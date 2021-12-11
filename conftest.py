443158d7a667bd25acd8058b5db225d6b26bafb7
loki <loki>
2021-12-11 16:58:08 +0500

    added login_page and locators

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ import pytest
 
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default=None,
+    parser.addoption('--language', action='store', default='ru',
                      help="Specify language")
 
 
